656add25fbad188682404a90a213abddace0157b
Derek Haller <dhaller.personal@gmail.com>
2018-01-26 01:57:19 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    if yield(colluection[i])
+    return colleciont[i] yield(colluection[i])
       return collections[i]
     end
     i+=1
